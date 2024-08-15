70a5bf2e95c7f0c4890e65be82c22f5ada44989d
Noel Chou <noel_chou@sil.org>
2024-08-15 12:40:05 -0400

    fix partially bolded typography

--- components/language-picker/react/language-chooser-react-mui/src/PartiallyBoldedTypography.tsx
+++ components/language-picker/react/language-chooser-react-mui/src/PartiallyBoldedTypography.tsx
@@ -5,67 +5,30 @@ import {
 } from "@ethnolib/find-language";
 import React from "react";
 
+function createNodesList(demarcatedText: string) {
+  const startMarkerSplitSegments = demarcatedText.split(START_OF_MATCH_MARKER);
+
+  const nodes = [];
+  for (const piece of startMarkerSplitSegments) {
+    const endMarkerSplitSegments = piece.split(END_OF_MATCH_MARKER);
+    if (endMarkerSplitSegments.length === 1) {
+      nodes.push(endMarkerSplitSegments[0]);
+    } else {
+      nodes.push(
+        <span style={{ fontWeight: "bold" }}>{endMarkerSplitSegments[0]}</span>
+      );
+      nodes.push(endMarkerSplitSegments[1]);
+    }
+  }
+  return nodes;
+}
+
 export const PartiallyBoldedTypography: React.FunctionComponent<
   {
-    dangerouslySetDemarcatedText: string;
+    demarcatedText: string;
   } & TypographyProps
-> = ({ dangerouslySetDemarcatedText, children, ...typographyProps }) => {
-  if (children) {
-    // Typography cannot take both children and dangerouslySetInnerHTML
-    console.error(
-      "PartiallyBoldedTypography does not support children. Put text content in the dangerouslySetDemarcatedText prop instead."
-    );
-  }
-  const htmlString = (dangerouslySetDemarcatedText ?? "")
-    .replaceAll(START_OF_MATCH_MARKER, "<><span style='font-weight: bold;'>") // needs ES2021
-    .replaceAll(END_OF_MATCH_MARKER, "</span><>");
-  const pieces = htmlString.split("<>");
+> = ({ demarcatedText, children, ...typographyProps }) => {
+  const nodes = createNodesList(demarcatedText);
 
-  //   return (
-  //     <Typography
-  //       dangerouslySetInnerHTML={{ __html: htmlString }}
-  //       {...typographyProps}
-  //     ></Typography>
-  //   );
-  // };
-
-  // e[ngl]ish
-  // e<span style='font-weight: bold;'>ngl</span>ish
-  //[<span>e</span>, <span style='font-weight: bold;'>ngl</span>, <span>ish</span>]
-  // [e, <span style='font-weight: bold;'>ngl</span>, ish]
-  // combine these all into a typography
-  // const textPiecesList = htmlString.split(
-  //   /<span style='font-weight: bold;'>|<\/span>/
-  // );
-  // const textPieces = textPiecesList.map((textPiece, index) => {
-  //   if (index % 2 === 0) {
-  //     return textPiece;
-  //   } else {
-  //     return <span style={{ fontWeight: "bold" }}>{textPiece}</span>;
-  //   }
-  // });
-  // const combinedTypography = (
-  //   <Typography {...typographyProps}>{...textPieces}</Typography>
-  // );
-  // TODO work on some implementation that doesn't use dangerouslySetInnerHTML
-  // don't forget to rename dangerouslySetDemarcatedText to something more appropriate
-  {
-    /* <Typography {...typographyProps}>
-    {htmlString.split(START_OF_MATCH_MARKER).map((part, index) => {
-      if (index === 0) {
-        return part;
-      }
-      const [markedPart, rest] = part.split(END_OF_MATCH_MARKER);
-      return (
-        <>
-          <span style={{ fontWeight: "bold" }}>{markedPart}</span>
-          {rest}
-        </>
-      );
-    })}
-    </Typography> */
-  }
-  // const pieces = htmlString.split(
-  // const a = ["a", <span style={{ fontWeight: "bold" }}>b</span>, "c"];
-  return <Typography>{...pieces}</Typography>;
+  return <Typography {...typographyProps}>{...nodes}</Typography>;
 };
