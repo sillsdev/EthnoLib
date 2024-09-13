316f4c0edb2c1a8e203a9755b74c1fbd4ea378bc
Noel Chou <noel_chou@sil.org>
2024-09-13 16:30:32 -0400

    vite config wip

--- components/language-picker/react/language-chooser-react-mui/vite.config.ts
+++ components/language-picker/react/language-chooser-react-mui/vite.config.ts
@@ -1,10 +1,41 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  root: __dirname,
   build: {
-    outDir: "dist",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "language-chooser-react-mui",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    sourcemap: true,
+    outDir:
+      "../../../../dist/components/language-picker/react/language-chooser-react-mui",
+    emptyOutDir: true,
   },
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    dts({
+      tsconfigPath: "./tsconfig.app.json",
+      entryRoot: "src",
+    }),
+  ],
 });
