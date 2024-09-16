0474a37be8191d0f5819f856dd3e3da3122d4055
Noel Chou <noel_chou@sil.org>
2024-09-16 18:47:30 -0400

    fix config based on nx generated vite and react lib

--- components/language-picker/react/language-chooser-react-mui/vite.config.ts
+++ components/language-picker/react/language-chooser-react-mui/vite.config.ts
@@ -1,41 +1,43 @@
-import path from "path";
+/// <reference types='vitest' />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
+import * as path from "path";
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
   root: __dirname,
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "language-chooser-react-mui",
-      fileName: (format) => `index.${format}.js`,
-      formats: ["es", "cjs", "umd"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    sourcemap: true,
-    outDir:
-      "../../../../dist/components/language-picker/react/language-chooser-react-mui",
-    emptyOutDir: true,
-  },
+  cacheDir:
+    "../../../../node_modules/.vite/components/language-picker/react/language-chooser-react-mui",
+
   plugins: [
     react(),
     nxViteTsPaths(),
     dts({
-      tsconfigPath: "./tsconfig.app.json",
       entryRoot: "src",
+      tsconfigPath: path.join(__dirname, "tsconfig.lib.json"),
     }),
   ],
+
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir:
+      "../../../../dist/components/language-picker/react/language-chooser-react-mui",
+
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      entry: "src/index.ts",
+      name: "@ethnolib/language-chooser-react-mui",
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+    },
+  },
 });
