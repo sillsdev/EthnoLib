c2a1d87c2fe75dbbab77c4e104d4409df3a549d5
Noel Chou <noel_chou@sil.org>
2024-09-20 12:01:48 -0400

    fix: fix dist output path?

--- components/language-picker/react/language-chooser-react-mui/vite.config.ts
+++ components/language-picker/react/language-chooser-react-mui/vite.config.ts
@@ -22,8 +22,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir:
-      "../../../../dist/components/language-picker/react/language-chooser-react-mui",
+    outDir: "../../../../dist/packages/language-chooser-react-mui",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
