3e04fdf7808f6ece01ad0f75ad0d3a20172d9933
Noel Chou <noel_chou@sil.org>
2024-09-26 10:43:03 -0400

    fix:make a change

--- components/language-picker/react/language-chooser-react-mui/src/LanguagePicker.tsx
+++ components/language-picker/react/language-chooser-react-mui/src/LanguagePicker.tsx
@@ -103,7 +103,7 @@ export const LanguagePicker: React.FunctionComponent<{
               font-weight: bold;
             `}
           >
-            Choose language
+            Choose languagexxxxxxxxxxxxxx
           </Typography>
         </Toolbar>
       </AppBar>
