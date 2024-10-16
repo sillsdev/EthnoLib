4f475ddab90d7d069d8b19f6c99b37a35c12c44d
Noel Chou <noel_chou@sil.org>
2024-10-16 15:19:56 -0400

    fix: BL-13987 subdialogs Just adding "fix:" to the commit message to trigger the build.

--- components/language-chooser/react/language-chooser-react-mui/src/CustomizeLanguageDialog.tsx
+++ components/language-chooser/react/language-chooser-react-mui/src/CustomizeLanguageDialog.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+/** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import * as React from "react";
 import {
   ICustomizableLanguageDetails,
   isUnlistedLanguage,
@@ -13,7 +14,9 @@ import {
   Dialog,
   DialogTitle,
   Typography,
+  Card,
 } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
 import { TextInput } from "./TextInput";
 import { COLORS } from "./colors";
 import {
@@ -25,6 +28,7 @@ import {
   stripDemarcation,
   createTag,
 } from "@ethnolib/find-language";
+import { FormFieldLabel } from "./FormFieldLabel";
 
 // ISO-3166-1 is a region code to region name lookup
 function getAllRegionOptions() {
@@ -76,6 +80,11 @@ export const CustomizeLanguageDialog: React.FunctionComponent<{
   const [dialogSelectedDialect, setDialogSelectedDialect] =
     React.useState<string>("");
 
+  // name (dialect) and country (region) are required for unlisted language
+  const isReadyToSubmit =
+    !isUnlistedLanguageDialog ||
+    (dialogSelectedDialect !== "" && dialogSelectedRegion.label !== "");
+
   // To reset the dialog if the user closes and reopens it, since they may have changed the language
   //or script selection in between
   React.useEffect(() => {
@@ -110,42 +119,76 @@ export const CustomizeLanguageDialog: React.FunctionComponent<{
       onClose={props.onClose}
       open={props.open}
       css={css`
-        padding: 25px;
+        .MuiDialog-paper {
+          padding: 20px 25px 25px 25px;
+          display: flex;
+          gap: 15px;
+        }
       `}
+      maxWidth={"sm"}
+      fullWidth={true}
     >
-      <DialogTitle>
+      <DialogTitle
+        css={css`
+          font-weight: bold;
+          padding: 0; //using padding on the entire dialog instead, plus gap between sections
+        `}
+      >
         {isUnlistedLanguageDialog
           ? "Unlisted Language Tag"
           : "Custom Language Tag"}
       </DialogTitle>
-      <DialogContent>
+      <DialogContent
+        css={css`
+          display: flex;
+          flex-direction: column;
+          gap: 15px;
+          padding: 0; //using padding on the entire dialog instead, plus gap between sections
+        `}
+      >
+        <Card
+          variant="outlined"
+          css={css`
+            border: 1px solid ${COLORS.blues[2]};
+            padding: 7px;
+            flex-shrink: 0;
+          `}
+        >
+          <Typography
+            css={css`
+              color: ${COLORS.blues[2]};
+              font-size: 0.875rem;
+              display: flex;
+              align-items: start;
+              gap: 7px;
+            `}
+          >
+            <InfoIcon />
+            {isUnlistedLanguageDialog
+              ? "If you cannot find a language and it does not appear in ethnologue.com, you can instead define the language here."
+              : "If you found the main language but need to change some of the specifics like Script or Dialect, you can do that here."}
+          </Typography>
+        </Card>
         {isUnlistedLanguageDialog && (
-          <div id="unlisted-language-dialog-content">
-            <TextInput
-              id="unlisted-lang-name-field"
-              label="Name"
-              value={dialogSelectedDialect}
-              onChange={(event) => {
-                setDialogSelectedDialect(event.target.value);
-              }}
-            />
-          </div>
+          <TextInput
+            id="unlisted-lang-name-field"
+            label="Name"
+            value={dialogSelectedDialect}
+            onChange={(event) => {
+              setDialogSelectedDialect(event.target.value);
+            }}
+            size={"small"}
+            css={css`
+              width: 100%;
+            `}
+            required={true}
+          />
         )}
-
         {!isUnlistedLanguageDialog && (
-          <div id="customize-language-dialog-content">
+          <div id="customize-script">
             {/* TODO future work: make these fuzzy search */}
-            <label htmlFor="customize-script-field">
-              <Typography
-                css={css`
-                  color: ${COLORS.greys[3]};
-                  font-weight: bold;
-                  margin-bottom: 5px;
-                `}
-              >
-                Script
-              </Typography>
-            </label>
+
+            <FormFieldLabel htmlFor="customize-script-field" label="Script" />
             <Autocomplete
               value={dialogSelectedScript}
               onChange={(
@@ -160,59 +203,64 @@ export const CustomizeLanguageDialog: React.FunctionComponent<{
               renderInput={(params) => (
                 <TextField {...params} id="customize-script-field" />
               )}
-            />
-            <label htmlFor="customize-region-field">
-              <Typography
-                css={css`
-                  color: ${COLORS.greys[3]};
-                  font-weight: bold;
-                  margin-bottom: 5px;
-                `}
-              >
-                Region
-              </Typography>
-            </label>
-            <Autocomplete
-              value={dialogSelectedRegion}
-              onChange={(
-                _event,
-                newValue: { label: string; id: string } | null
-              ) => {
-                setDialogSelectedRegion(newValue || EMPTY_COMBOBOX_VALUE);
-              }}
-              disablePortal
-              id="combo-box-language-chooser-react-mui"
-              options={getAllRegionOptions()}
-              renderInput={(params) => (
-                <TextField {...params} id="customize-region-field" />
-              )}
-            />
-            {/* TODO future work: make this also a autocomplete with registered variants */}
-            {/* use ROLV from langtags repo? */}
-            {/* // from BCP-47 https://www.rfc-editor.org/bcp/bcp47.txt:
-          // 4.  Variant subtags MUST be registered with IANA according to the
-          // rules in Section 3.5 of this document before being used to form
-          // language tags.  In order to distinguish variants from other types
-          // of subtags, registrations MUST meet the following length and
-          // content restrictions:
-
-          // 1.  Variant subtags that begin with a letter (a-z, A-Z) MUST be
-          //     at least five characters long.  */}
-            <TextInput
-              id="customize-variant-field"
-              label="Variant (dialect)"
-              value={dialogSelectedDialect}
-              onChange={(event) => {
-                setDialogSelectedDialect(event.target.value);
-              }}
-              inputProps={{
-                spellCheck: false,
-              }}
+              size={"small"}
             />
           </div>
         )}
+
+        <div id="customize-region">
+          <FormFieldLabel
+            htmlFor="customize-region-field"
+            label="Country"
+            required={isUnlistedLanguageDialog}
+          />
+          <Autocomplete
+            value={dialogSelectedRegion}
+            onChange={(
+              _event,
+              newValue: { label: string; id: string } | null
+            ) => {
+              setDialogSelectedRegion(newValue || EMPTY_COMBOBOX_VALUE);
+            }}
+            disablePortal
+            id="combo-box-language-chooser-react-mui"
+            options={getAllRegionOptions()}
+            renderInput={(params) => (
+              <TextField {...params} id="customize-region-field" />
+            )}
+            size={"small"}
+          />
+        </div>
+        {!isUnlistedLanguageDialog && (
+          <TextInput
+            // {/* TODO future work: make this also a autocomplete with registered variants */}
+            // {/* use ROLV from langtags repo? */}
+            // {/* // from BCP-47 https://www.rfc-editor.org/bcp/bcp47.txt:
+            //   // 4.  Variant subtags MUST be registered with IANA according to the
+            //   // rules in Section 3.5 of this document before being used to form
+            //   // language tags.  In order to distinguish variants from other types
+            //   // of subtags, registrations MUST meet the following length and
+            //   // content restrictions:
+
+            //   // 1.  Variant subtags that begin with a letter (a-z, A-Z) MUST be
+            //   //     at least five characters long.  */}
+            id="customize-variant-field"
+            label="Variant (dialect)"
+            value={dialogSelectedDialect}
+            onChange={(event) => {
+              setDialogSelectedDialect(event.target.value);
+            }}
+            size={"small"}
+            css={css`
+              width: 100%;
+            `}
+            inputProps={{
+              spellCheck: false,
+            }}
+          />
+        )}
         <Typography>
-          Tag:
+          BCP 47 Tag:{" "}
           <span
             css={css`
               font-weight: bold;
@@ -230,7 +278,11 @@ export const CustomizeLanguageDialog: React.FunctionComponent<{
         </Typography>
       </DialogContent>
       {/* // TODO abstract out these buttons which are copied from app.tsx */}
-      <DialogActions>
+      <DialogActions
+        css={css`
+          padding: 0; //using padding on the entire dialog instead, plus gap between sections
+        `}
+      >
         <div
           id="buttons-container"
           css={css`
@@ -238,7 +290,6 @@ export const CustomizeLanguageDialog: React.FunctionComponent<{
             width: 100%;
             display: flex;
             justify-content: flex-end;
-            padding-top: 15px;
           `}
         >
           <Button
@@ -249,6 +300,7 @@ export const CustomizeLanguageDialog: React.FunctionComponent<{
             `}
             variant="contained"
             color="primary"
+            disabled={!isReadyToSubmit}
             onClick={() => {
               if (isUnlistedLanguageDialog) {
                 props.selectUnlistedLanguage();
