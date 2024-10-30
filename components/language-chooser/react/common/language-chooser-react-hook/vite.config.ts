462aae178863b8d75cd82190e7d291afe34f8f83
Noel Chou <noel_chou@sil.org>
2024-10-30 14:27:50 -0400

    fix: make hook vite and publishable

---
+++ components/language-chooser/react/common/language-chooser-react-hook/vite.config.ts
@@ -0,0 +1,41 @@
+/// <reference types='vitest' />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import * as path from "path";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir:
+    "../../../../../node_modules/.vite/components/language-chooser/react/common/language-chooser-react-hook",
+
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    dts({
+      entryRoot: "src",
+      tsconfigPath: path.join(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
+
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      entry: "./index.ts",
+      name: "@ethnolib/language-chooser-react-hook",
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+    },
+  },
+});
