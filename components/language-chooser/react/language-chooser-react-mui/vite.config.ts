4c8189d0816972a16485fabb5a5e15aff26b1d8b
Noel Chou <noel_chou@sil.org>
2025-01-15 14:23:59 -0500

    externalize mui+

--- components/language-chooser/react/language-chooser-react-mui/vite.config.ts
+++ components/language-chooser/react/language-chooser-react-mui/vite.config.ts
@@ -35,7 +35,14 @@ export default defineConfig({
       formats: ["es", "cjs"],
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: [
+        "@emotion/react",
+        // Externalizing "@emotion/styled" broke the package. I didn't figure out why
+        "@mui/material",
+        "react",
+        "react-dom",
+        "react/jsx-runtime",
+      ],
     },
   },
   server: {
