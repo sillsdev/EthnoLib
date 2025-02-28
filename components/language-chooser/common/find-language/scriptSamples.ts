439f771aea4466ce2b4552cb86ae5fd86a3e9e33
Noel Chou <noel_chou@sil.org>
2025-02-28 13:14:40 -0500

    fix: BL-14325 add script samples for major indian scripts

--- components/language-chooser/common/find-language/scriptSamples.ts
+++ components/language-chooser/common/find-language/scriptSamples.ts
@@ -1,11 +1,22 @@
 export const scriptSamples: { [key: string]: string } = {
+  // Scripts which frequently show up as options for languages with multiple scripts
   latn: "A B C D",
   arab: "ا ب ت ث",
-  deva: "अ आ इ ई",
+  deva: "क ख ग घ",
   cyrl: "А Б В Г",
   tibt: "ཀ ཁ ག གྷ",
   ethi: "ሀ ሁ ሂ ሃ",
   thai: "ก ข ค ฆ",
-  beng: "অ আ ই ঈ",
+  beng: "ক খ গ ঘ",
   mymr: "က ခ ဂ ဃ",
+
+  // Some additional Indian scripts
+  guru: "ਕ ਖ ਗ ਘ",
+  gujr: "ક ખ ગ ઘ",
+  taml: "க ங ச ஞ",
+  telu: "క ఖ గ ఘ",
+  knda: "ಕ ಖ ಗ ಘ",
+  mlym: "ക ഖ ഗ ഘ",
+  orya: "କ ଖ ଗ ଘ",
+  mtei: "ꯀ ꯈ ꯒ ꯔ",
 };
