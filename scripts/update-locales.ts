92f11d83a6dbb86f3bb123f20fb59061be798182
Andrew Polk <andrew_polk@sil.org>
2025-03-11 15:18:29 -0700

    fix: update-locales script

--- scripts/update-locales.ts
+++ scripts/update-locales.ts
@@ -3,13 +3,8 @@
 // See README-l10n.md for more information.
 // (This was mostly created by copilot.)
 
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Get current directory in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const fs = require("fs");
+const path = require("path");
 
 // Path to locales directory and JSON output file
 const localesDir = path.resolve(__dirname, "../locales");
